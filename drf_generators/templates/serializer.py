a623e3559a8bc7320adbebc8c9e4971512f244d3
josealbertodejsalmanzar <josealbertodejsalmanzar@gmail.com>
2022-03-15 23:02:47 -0400

    Update serializer.py

--- drf_generators/templates/serializer.py
+++ drf_generators/templates/serializer.py
@@ -2,7 +2,9 @@ __all__ = ['SERIALIZER']
 
 
 SERIALIZER = """from rest_framework.serializers import ModelSerializer
-from {{ app }}.models import {{ models | join:', ' }}
+from {{ app }}.models import (
+    {{ models | join:',' }}
+)
 {% for model in models %}
 
 class {{ model }}Serializer(ModelSerializer):
